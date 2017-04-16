8c418d338179a7efcbdadcbcb2be71a15a398bbc
yyfan <yyfan1994@gmail.com>
2017-04-16 13:50:46 -0700

    second commit

--- simple-calc/ViewController.swift
+++ simple-calc/ViewController.swift
@@ -13,7 +13,30 @@ class ViewController: UIViewController {
     let screenWidth = UIScreen.main.bounds.width
     let screenHeight = UIScreen.main.bounds.height
 
-    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var btn0: UIButton!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var subBtn: UIButton!
+    @IBOutlet weak var mulBtn: UIButton!
+    @IBOutlet weak var didBtn: UIButton!
+    @IBOutlet weak var modBtn: UIButton!
+    @IBOutlet weak var pntBtn: UIButton!
+    @IBOutlet weak var cntBtn: UIButton!
+    @IBOutlet weak var avgBtn: UIButton!
+    @IBOutlet weak var FactBtn: UIButton!
+    @IBOutlet weak var eqlBtn: UIButton!
+    
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ simple-calc/roundButton.swift
@@ -0,0 +1,21 @@
+//
+//  roundButton.swift
+//  simple-calc
+//
+//  Created by iGuest on 4/16/17.
+//  Copyright © 2017 yyfan. All rights reserved.
+//
+
+import UIKit
+
+class roundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
